27a8c3e4241fdfe26f6d84e538d5bde50d145846
Mihai <mihai.ocneanu@gmail.com>
2020-04-02 11:29:50 +0300

    added dependency, updated form

--- src/bulma/pages/products/components/BaseForm.vue
+++ src/bulma/pages/products/components/BaseForm.vue
@@ -1,6 +1,13 @@
 <template>
     <enso-form class="box form-box has-background-light raises-on-hover"
         @ready="form = $event.form">
+        <template v-slot:category_id="{ field }">
+            <label class="label">
+                {{ i18n('Category') }}
+            </label>
+            <categories v-model="field.value"
+                ref="categories"/>
+        </template>
         <template v-slot:suppliers>
             <div class="wrapper is-block">
                 <div class="columns">
@@ -24,12 +31,15 @@
 
 <script>
 import { EnsoForm, FormField } from '@enso-ui/forms/bulma';
+import Categories from '@enso-ui/categories/src/bulma/pages/administration/categories/components/Categories.vue';
 import Supplier from './Supplier.vue';
 
 export default {
     name: 'BaseForm',
 
-    components: { EnsoForm, FormField, Supplier },
+    components: {
+        Categories, EnsoForm, FormField, Supplier,
+    },
 
     inject: ['i18n'],
 
