872472a7652855a820aca60dab2a4a71ab3d2eec
Mihai <mihai.ocneanu@gmail.com>
2021-05-18 16:10:31 +0300

    refactored; added newly computed supplier discounted price

--- src/bulma/pages/products/Edit.vue
+++ src/bulma/pages/products/Edit.vue
@@ -1,34 +1,32 @@
 <template>
-    <div class="columns is-centered">
-        <div class="column is-three-quarters-desktop is-full-touch">
-            <base-form/>
-            <accessories>
-                <template slot-scope="{ count }">
-                    <tab keep-alive
-                         id="Comments">
-                        <div class="columns is-centered">
-                            <div class="column is-two-thirds">
-                                <comments :id="id"
-                                    type="product"
-                                    @update="$set(count, 'Comments', $refs.comments.count)"
-                                    ref="comments"/>
-                            </div>
+    <div class="wrapper">
+        <base-form/>
+        <accessories>
+            <template slot-scope="{ count }">
+                <tab keep-alive
+                    id="Comments">
+                    <div class="columns is-centered">
+                        <div class="column is-two-thirds">
+                            <comments :id="id"
+                                type="product"
+                                @update="$set(count, 'Comments', $refs.comments.count)"
+                                ref="comments"/>
                         </div>
-                    </tab>
-                    <tab keep-alive
-                         id="Documents">
-                        <div class="columns is-centered">
-                            <div class="column is-two-thirds">
-                                <documents :id="id"
-                                    type="product"
-                                    @update="$set(count, 'Documents', $refs.documents.count)"
-                                    ref="documents"/>
-                            </div>
+                    </div>
+                </tab>
+                <tab keep-alive
+                    id="Documents">
+                    <div class="columns is-centered">
+                        <div class="column is-two-thirds">
+                            <documents :id="id"
+                                type="product"
+                                @update="$set(count, 'Documents', $refs.documents.count)"
+                                ref="documents"/>
                         </div>
-                    </tab>
-                </template>
-            </accessories>
-        </div>
+                    </div>
+                </tab>
+            </template>
+        </accessories>
     </div>
 </template>
 
